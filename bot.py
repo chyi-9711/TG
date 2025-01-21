4b423de8b975639205967ca6b18414c1f20f04d6
chyi-9711 <chen.mina53388@gmail.com>
2025-01-22 01:08:18 +0800

    init commit

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton,CallbackQuery
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 import json
-TOKEN="8101101098:AAFTKiKBQxrwS3X7bJ3mzvLjsKU6auTcKKY"
+TOKEN="8101101098:AAFuHYiZfuSgrc0-5JUWqjn9nvtBFkUo-Y4"
 bot=telebot.TeleBot(TOKEN,parse_mode=None)
 user_passwords = {}
 def load_txt():
@@ -14,14 +14,15 @@ txt = load_txt()
 def send_txt(message):
     bot.send_message(message.chat.id, "大家好啊我是豆沙包，不知道為什麼明明是寒假我還要可撥的被人類壓榨嗚嗚嗚")
     bot.send_message(message.chat.id, "接下來的這趟旅途我會一直陪伴大家，也會不斷給各位發布任務和提示，接下來是RPG遊戲規則介紹，要仔細閱讀喔！")
-    bot.send_message(message.chat.id, "1.這次的遊戲會直接給玩家分發地圖自由活動，每個場地都可以重複過去遊玩，至少要幫npc完成一個任務才能擁有線索，也可以積累積分點，根據任務完成度不同，可以獲得的線索數量也不同，當然，也可以重複挑戰任務，但不可以連續重複挑戰同一角色的任務。")
+    bot.send_message(message.chat.id, "1.這次的遊戲會直接給玩家分發地圖自由活動，每個場地都可以重複過去遊玩，至少要幫npc完成一個任務才能擁有線索，也可以積累積分點，根據任務完成度不同，可以獲得的線索數量也不同，當然，也可以重複挑戰任務，但不可以連續重複挑戰同一角色的任務")
     bot.send_message(message.chat.id, "2.線索一次只能選一個地方拿，例如教室和辦公室，雖然同樣在學校，但一次只能選一個地方")
     bot.send_message(message.chat.id, "3.拿到道具(線索)後可以選擇拍照傳群組，不可以拿走道具，每得到一個線索，都會有一個提示詞，只要輸入提示即可從機器人身上拿到完整劇情線索")
-    bot.send_message(message.chat.id, "4.遊玩過程會不定時啟動臨時任務，完成任務可以獲得積分點。")
-    bot.send_message(message.chat.id, "5.積分點除了在最後結算有加成外，也可以拿來買線索。")
+    bot.send_message(message.chat.id, "4.遊玩過程會不定時啟動臨時任務，完成任務可以獲得積分點")
+    bot.send_message(message.chat.id, "5.積分點除了在最後結算有加成外，也可以拿來購買線索")
     bot.send_message(message.chat.id, "6.每小都有一次對外求助機會，積分點達到一定數額也可以兌換求助，求助內容是關於劇情推測相關的內容")
-    bot.send_message(message.chat.id, "7.積分點除了在最後結算有加成外，也可以拿來買線索。")
+    bot.send_message(message.chat.id, "7.如果有需要的話，可以隨時給阿潛發訊息或打電話，在搜索過程中他會不停走動，想找到他也是有難度的喔！")
     bot.send_message(message.chat.id, "8.遊戲時間是有限制的，時間到便會強制結束，並發放劇情相關題目，真相及結局選擇會等所有小隊作答完畢後統一發放")
+    bot.send_message(message.chat.id, "9.我們的最終結算包含積分、劇情選擇、問卷，問卷部分會詢問有關於劇情的問題，所以一定要還原故事真相喔！")
     bot.send_message(message.chat.id, "豆沙包就先在這裡祝大家玩得愉快啦！這次要進入的故事名叫「雙生花」，是可憐的豆沙包趕工出來的遊戲，不要嫌棄它不然我會很杯桑喔！")
 
 @bot.message_handler(commands=['開始遊戲'])
